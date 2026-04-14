f7fee8726dfc6c1c94e5bc1d9f3533a2d9310875
Matt Denwood <md@sund.ku.dk>
2026-04-14 15:16:28 +0200

    Basic template for fecrt_sim function

---
+++ R/fecrt_sim.R
@@ -0,0 +1,18 @@
+#' Title
+#'
+#' @details
+#' Allows for:
+#'  - treatment/control or pre/post treatment
+#'  - sample size of animals
+#'  - pools of P animals
+#'  - pasture collections
+#'
+#'
+#'
+#' @param n_animals the number of animals to simulate
+#'
+#' @export
+fecrt_sim <- function(n_animals){
+
+  return("hello world")
+}
