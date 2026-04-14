afb5611d59caa4c9f24cbdca7b1d058395f598d8
Matt Denwood <md@sund.ku.dk>
2026-04-14 15:37:16 +0200

    Installation script

--- R/fecrt_sim.R
+++ R/fecrt_sim.R
@@ -8,8 +8,11 @@
 #'  - pasture collections
 #'
 #'
-#'
-#' @param n_animals the number of animals to simulate
+#' @param n_sim the number of FECRT to simulate
+#' @param design either paired or unpaired
+#' @param n_treatment the number of animals in the treatment group
+#' @param n_control the number of animals in the control group (ignored for paired design)
+#' @param n_pools the number of pooled samples
 #'
 #' @export
 fecrt_sim <- function(n_animals){
---
+++ drafts/installation.R
@@ -0,0 +1,11 @@
+## First install the dependent packages (if necessary):
+install.packages(c("Rcpp","bayescount","rlang","dplyr","tibble","knitr","rmarkdown","spelling","testthat"))
+
+## Then install the package by clicking on "Install" at the top right
+
+## Then load the package:
+library("fecrtsim")
+
+## And use the functions:
+fecrt_sim()
+
